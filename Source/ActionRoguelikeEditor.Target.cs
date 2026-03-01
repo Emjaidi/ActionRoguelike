10ce00967472788a90dd5cffa8c001579c87be26
MJD <madelas797@proton.me>
2026-03-01 18:00:07 -0500

    revert target cs engine version to 5.6

--- Source/ActionRoguelikeEditor.Target.cs
+++ Source/ActionRoguelikeEditor.Target.cs
@@ -8,8 +8,7 @@ public class ActionRoguelikeEditorTarget : TargetRules
 	public ActionRoguelikeEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "ActionRoguelike" } );
 	}
