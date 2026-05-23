00ec839716fa3958cd4af8a2f76cd4a1d5ec5b4e
MJD <madelas797@proton.me>
2026-05-23 15:01:11 -0400

    Data oriented design code addition Focusing on coin pickups

---
+++ Source/ActionRoguelike/Pickups/RogueCoinPickupSubsystem.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RogueCoinPickupSubsystem.h"
+
+void URogueCoinPickupSubsystem::AddCoinPickups(TArray<FVector> NewLocation, TArray<int32> NewAmounts)
+{
+	
+	CoinLocations.Append(NewLocation);
+	CoinAmounts.Append(NewAmounts);
+}
+
+void URogueCoinPickupSubsystem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	UWorld* World = GetWorld();
+
+	for (int i = 0; i < CoinLocations.Num(); ++i)
+	{
+		DrawDebugPoint(World, CoinLocations[i], 8.0f, FColor::White);
+	}
+}
---
+++ Source/ActionRoguelike/Pickups/RogueCoinPickupSubsystem.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "RogueCoinPickupSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API URogueCoinPickupSubsystem : public UTickableWorldSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	void AddCoinPickups(TArray<FVector> NewLocation, TArray<int32> NewAmounts);
+	
+	void Tick(float DeltaTime) override;
+	virtual TStatId GetStatId() const override
+	{
+		RETURN_QUICK_DECLARE_CYCLE_STAT(FBehaviorTreeEditorTickHelper, STATGROUP_Tickables); 
+	}
+protected:
+	TArray<FVector> CoinLocations;
+	TArray<int32> CoinAmounts;
+	
+};
